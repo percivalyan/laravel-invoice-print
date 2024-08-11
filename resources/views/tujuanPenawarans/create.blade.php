00c751d2fe2f5d8c6afc13b86e49d0459a9d9e71
percivalyan <yoryan8888@gmail.com>
2024-08-12 00:43:31 +0700

    fix tujuan

--- resources/views/tujuanPenawarans/create.blade.php
+++ resources/views/tujuanPenawarans/create.blade.php
@@ -1,57 +1,32 @@
-{{-- @extends('layouts.app')
-
-@section('content')
-    <h1>Create Tujuan Penawaran</h1>
-
-    @if ($errors->any())
-        <div>
-            <ul>
-                @foreach ($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                @endforeach
-            </ul>
-        </div>
-    @endif
-
-    <form action="{{ route('tujuanPenawarans.store') }}" method="POST">
-        @csrf
-
-        <label for="project_penawaran_id">Project Penawaran:</label>
-        <select name="project_penawaran_id" id="project_penawaran_id">
-            @foreach ($projectPenawarans as $projectPenawaran)
-                <option value="{{ $projectPenawaran->id }}">{{ $projectPenawaran->proyek }}</option>
-            @endforeach
-        </select>
-
-        <label for="pengajuan">Pengajuan:</label>
-        <input type="text" name="pengajuan" id="pengajuan" value="{{ old('pengajuan') }}">
-
-        <label for="tujuan">Tujuan:</label>
-        <input type="text" name="tujuan" id="tujuan" value="{{ old('tujuan') }}">
-
-        <button type="submit">Create</button>
-    </form>
-@endsection --}}
-
-<!-- resources/views/tujuanPenawarans/create.blade.php -->
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <h1>Create Tujuan Penawaran</h1>
-    <form action="{{ route('tujuanPenawarans.store') }}" method="POST">
-        @csrf
-        <input type="hidden" name="project_penawaran_id" value="{{ $projectPenawaran->id }}">
-        <!-- other fields... -->
-        <div class="form-group">
-            <label for="pengajuan">Pengajuan</label>
-            <input type="text" class="form-control" id="pengajuan" name="pengajuan" value="{{ old('pengajuan') }}">
+<div class="container-fluid">
+    <!-- Page Heading -->
+    <div class="d-sm-flex align-items-center justify-content-between mb-4">
+        <h1 class="h3 mb-0 text-gray-800">Create Tujuan Penawaran</h1>
+    </div>
+
+    <!-- Form Card -->
+    <div class="card shadow mb-4">
+        <div class="card-header py-3">
+            <h6 class="m-0 font-weight-bold text-primary">Create Tujuan Penawaran</h6>
         </div>
-        <div class="form-group">
-            <label for="tujuan">Tujuan</label>
-            <input type="text" class="form-control" id="tujuan" name="tujuan" value="{{ old('tujuan') }}">
+        <div class="card-body">
+            <form action="{{ route('tujuanPenawarans.store') }}" method="POST">
+                @csrf
+                <input type="hidden" name="project_penawaran_id" value="{{ $projectPenawaran->id }}">
+                <div class="form-group">
+                    <label for="pengajuan">Pengajuan</label>
+                    <input type="text" class="form-control" id="pengajuan" name="pengajuan" value="{{ old('pengajuan') }}" required>
+                </div>
+                <div class="form-group">
+                    <label for="tujuan">Tujuan</label>
+                    <input type="text" class="form-control" id="tujuan" name="tujuan" value="{{ old('tujuan') }}" required>
+                </div>
+                <button type="submit" class="btn btn-primary">Submit</button>
+            </form>
         </div>
-        <button type="submit" class="btn btn-primary">Submit</button>
-    </form>
+    </div>
 </div>
 @endsection
--- resources/views/tujuanPenawarans/edit.blade.php
+++ resources/views/tujuanPenawarans/edit.blade.php
@@ -1,24 +1,36 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <h1>Edit Tujuan Penawaran</h1>
-    <form action="{{ route('tujuanPenawarans.update', $tujuanPenawaran->id) }}" method="POST">
-        @csrf
-        @method('PUT')
-        <div class="form-group">
-            <label for="project_penawaran">Project Penawaran</label>
-            <input type="text" class="form-control" id="project_penawaran" name="project_penawaran" value="{{ $tujuanPenawaran->projectPenawaran->proyek }}" readonly>
+<div class="container-fluid">
+    <!-- Page Heading -->
+    <div class="d-sm-flex align-items-center justify-content-between mb-4">
+        <h1 class="h3 mb-0 text-gray-800">Edit Tujuan Penawaran</h1>
+    </div>
+
+    <!-- Form Card -->
+    <div class="card shadow mb-4">
+        <div class="card-header py-3">
+            <h6 class="m-0 font-weight-bold text-primary">Edit Tujuan Penawaran</h6>
         </div>
-        <div class="form-group">
-            <label for="pengajuan">Pengajuan</label>
-            <input type="text" class="form-control" id="pengajuan" name="pengajuan" value="{{ old('pengajuan', $tujuanPenawaran->pengajuan) }}">
+        <div class="card-body">
+            <form action="{{ route('tujuanPenawarans.update', $tujuanPenawaran->id) }}" method="POST">
+                @csrf
+                @method('PUT')
+                <div class="form-group">
+                    <label for="project_penawaran">Project Penawaran</label>
+                    <input type="text" class="form-control" id="project_penawaran" name="project_penawaran" value="{{ $tujuanPenawaran->projectPenawaran->proyek }}" readonly>
+                </div>
+                <div class="form-group">
+                    <label for="pengajuan">Pengajuan</label>
+                    <input type="text" class="form-control" id="pengajuan" name="pengajuan" value="{{ old('pengajuan', $tujuanPenawaran->pengajuan) }}" required>
+                </div>
+                <div class="form-group">
+                    <label for="tujuan">Tujuan</label>
+                    <input type="text" class="form-control" id="tujuan" name="tujuan" value="{{ old('tujuan', $tujuanPenawaran->tujuan) }}" required>
+                </div>
+                <button type="submit" class="btn btn-primary">Update</button>
+            </form>
         </div>
-        <div class="form-group">
-            <label for="tujuan">Tujuan</label>
-            <input type="text" class="form-control" id="tujuan" name="tujuan" value="{{ old('tujuan', $tujuanPenawaran->tujuan) }}">
-        </div>
-        <button type="submit" class="btn btn-primary">Update</button>
-    </form>
+    </div>
 </div>
 @endsection
