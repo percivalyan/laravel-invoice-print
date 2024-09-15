1f18a378b9fd897d025c8fe16c99bc1e753ccc9c
percivalyan <yoryan8888@gmail.com>
2024-09-16 00:43:29 +0700

    main: Updated update icon "Tambahkan Pekerjaan"

--- resources/views/projectKwitansis/index.blade.php
+++ resources/views/projectKwitansis/index.blade.php
@@ -5,7 +5,8 @@
         <!-- Page Heading -->
         <div class="d-flex justify-content-between align-items-center mb-4">
             <h1 class="h3 mb-0 text-gray-800">Daftar Project Kwitansi</h1>
-            <a href="{{ route('projectKwitansis.create') }}" class="btn btn-primary"><i class="fas fa-plus"></i> Tambah Project Kwitansi</a>
+            <a href="{{ route('projectKwitansis.create') }}" class="btn btn-primary"><i class="fas fa-plus"></i> Tambah Project
+                Kwitansi</a>
         </div>
 
         <!-- Pencarian dan Sorting -->
@@ -16,15 +17,22 @@
                         <select name="filter" class="form-select">
                             <option value="">Pilih Filter</option>
                             <option value="all" {{ request('filter') == 'all' ? 'selected' : '' }}>Cari Semua</option>
-                            <option value="kepada_yth" {{ request('filter') == 'kepada_yth' ? 'selected' : '' }}>Client</option>
+                            <option value="kepada_yth" {{ request('filter') == 'kepada_yth' ? 'selected' : '' }}>Client
+                            </option>
                             <option value="proyek" {{ request('filter') == 'proyek' ? 'selected' : '' }}>Proyek</option>
-                            <option value="project_pembelian.nomor_po" {{ request('filter') == 'project_pembelian.nomor_po' ? 'selected' : '' }}>Reff PO No</option>
-                            <option value="nomor_surat_jalan" {{ request('filter') == 'nomor_surat_jalan' ? 'selected' : '' }}>Nomor Surat Jalan</option>
-                            <option value="nomor_invoice" {{ request('filter') == 'nomor_invoice' ? 'selected' : '' }}>Nomor Invoice</option>
-                            <option value="nomor_bast" {{ request('filter') == 'nomor_bast' ? 'selected' : '' }}>Nomor BAST</option>
+                            <option value="project_pembelian.nomor_po"
+                                {{ request('filter') == 'project_pembelian.nomor_po' ? 'selected' : '' }}>Reff PO No
+                            </option>
+                            <option value="nomor_surat_jalan"
+                                {{ request('filter') == 'nomor_surat_jalan' ? 'selected' : '' }}>Nomor Surat Jalan</option>
+                            <option value="nomor_invoice" {{ request('filter') == 'nomor_invoice' ? 'selected' : '' }}>Nomor
+                                Invoice</option>
+                            <option value="nomor_bast" {{ request('filter') == 'nomor_bast' ? 'selected' : '' }}>Nomor BAST
+                            </option>
                             <option value="lokasi" {{ request('filter') == 'lokasi' ? 'selected' : '' }}>Lokasi</option>
                         </select>
-                        <input type="text" name="keyword" class="form-control" placeholder="Cari..." value="{{ request('keyword') }}">
+                        <input type="text" name="keyword" class="form-control" placeholder="Cari..."
+                            value="{{ request('keyword') }}">
                         <button type="submit" class="btn btn-primary"><i class="fas fa-search"></i></button>
                     </div>
                 </div>
@@ -32,7 +40,8 @@
                 <div class="col-md-6">
                     <select name="sort" class="form-select" onchange="this.form.submit()">
                         <option value="">Sort by</option>
-                        <option value="kepada_yth" {{ request('sort') == 'kepada_yth' ? 'selected' : '' }}>Client (A-Z)</option>
+                        <option value="kepada_yth" {{ request('sort') == 'kepada_yth' ? 'selected' : '' }}>Client (A-Z)
+                        </option>
                         <option value="proyek" {{ request('sort') == 'proyek' ? 'selected' : '' }}>Proyek (A-Z)</option>
                         <option value="lokasi" {{ request('sort') == 'lokasi' ? 'selected' : '' }}>Lokasi (A-Z)</option>
                     </select>
@@ -59,9 +68,7 @@
                                 <th>Client</th>
                                 <th>Proyek</th>
                                 <th>Reff PO No</th>
-                                <th>Nomor Surat Jalan</th>
-                                <th>Nomor Invoice</th>
-                                <th>Nomor BAST</th>
+                                <th>Informasi Dokumen</th>
                                 <th>Lokasi</th>
                                 <th>Surat</th>
                                 <th>Aksi</th>
@@ -73,39 +80,95 @@
                                     <td>{{ $loop->iteration }}</td>
                                     <td>{{ $projectKwitansi->kepada_yth }}</td>
                                     <td>{{ $projectKwitansi->proyek }}</td>
-                                    <td>{{ $projectKwitansi->projectPembelian ? $projectKwitansi->projectPembelian->nomor_po : 'N/A' }}</td>
-                                    <td>{{ $projectKwitansi->nomor_surat_jalan }}</td>
-                                    <td>{{ $projectKwitansi->nomor_invoice }}</td>
-                                    <td>{{ $projectKwitansi->nomor_bast }}</td>
+                                    <td>{{ $projectKwitansi->projectPembelian ? $projectKwitansi->projectPembelian->nomor_po : 'N/A' }}
+                                    </td>
+                                    <td>
+                                        <div>
+                                            <strong>Nomor Surat Jalan:</strong>
+                                            {{ $projectKwitansi->nomor_surat_jalan }}<br>
+                                            <strong>Nomor Invoice:</strong> {{ $projectKwitansi->nomor_invoice }}<br>
+                                            <strong>Nomor BAST:</strong> {{ $projectKwitansi->nomor_bast }}
+                                        </div>
+                                    </td>
                                     <td>{{ $projectKwitansi->lokasi }}</td>
                                     <td class="text-center">
                                         <div class="dropdown">
-                                            <button class="btn btn-info btn-sm dropdown-toggle" type="button" id="dropdownMenuSurat{{ $projectKwitansi->id }}" data-bs-toggle="dropdown" aria-expanded="false">
+                                            <button class="btn btn-info btn-sm dropdown-toggle" type="button"
+                                                id="dropdownMenuSurat{{ $projectKwitansi->id }}" data-bs-toggle="dropdown"
+                                                aria-expanded="false">
                                                 Surat
                                             </button>
-                                            <ul class="dropdown-menu" aria-labelledby="dropdownMenuSurat{{ $projectKwitansi->id }}">
-                                                <li><a class="dropdown-item" href="{{ route('projectKwitansis.show', $projectKwitansi->id) }}"><i class="fas fa-file-alt"></i> Surat Jalan</a></li>
-                                                <li><a class="dropdown-item" href="{{ route('projectKwitansis.showinvoice', $projectKwitansi->id) }}"><i class="fas fa-file-invoice"></i> Invoice</a></li>
-                                                <li><a class="dropdown-item" href="{{ route('projectKwitansis.showbast', $projectKwitansi->id) }}"><i class="fas fa-file-signature"></i> BAST</a></li>
+                                            <ul class="dropdown-menu"
+                                                aria-labelledby="dropdownMenuSurat{{ $projectKwitansi->id }}">
+                                                <li><a class="dropdown-item"
+                                                        href="{{ route('projectKwitansis.show', $projectKwitansi->id) }}"><i
+                                                            class="fas fa-file-alt"></i> Surat Jalan</a></li>
+                                                <li><a class="dropdown-item"
+                                                        href="{{ route('projectKwitansis.showinvoice', $projectKwitansi->id) }}"><i
+                                                            class="fas fa-file-invoice"></i> Invoice</a></li>
+                                                <li><a class="dropdown-item"
+                                                        href="{{ route('projectKwitansis.showbast', $projectKwitansi->id) }}"><i
+                                                            class="fas fa-file-signature"></i> BAST</a></li>
                                             </ul>
                                         </div>
                                     </td>
                                     <td class="text-center">
                                         <div class="dropdown">
-                                            <button class="btn btn-secondary btn-sm dropdown-toggle" type="button" id="dropdownMenuButton{{ $projectKwitansi->id }}" data-bs-toggle="dropdown" aria-expanded="false">
+                                            <button class="btn btn-secondary btn-sm dropdown-toggle" type="button"
+                                                id="dropdownMenuButton{{ $projectKwitansi->id }}" data-bs-toggle="dropdown"
+                                                aria-expanded="false">
                                                 Aksi
                                             </button>
-                                            <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton{{ $projectKwitansi->id }}">
-                                                <li><a class="dropdown-item" href="{{ route('projectKwitansis.edit', $projectKwitansi->id) }}"><i class="fas fa-edit"></i> Edit</a></li>
-                                                <li><a class="dropdown-item" href="{{ route('catatanKwitansis.create', ['project_kwitansi_id' => $projectKwitansi->id]) }}"><i class="fas fa-sticky-note"></i> Catatan</a></li>
-                                                <li><a class="dropdown-item" href="{{ route('pekerjaanKwitansis.index', ['project_kwitansi_id' => $projectKwitansi->id]) }}">Tambahkan Pekerjaan</a></li>
+                                            <ul class="dropdown-menu"
+                                                aria-labelledby="dropdownMenuButton{{ $projectKwitansi->id }}">
                                                 <li>
-                                                    <form action="{{ route('projectKwitansis.destroy', $projectKwitansi->id) }}" method="POST" style="display:inline;">
+                                                    <a class="dropdown-item"
+                                                        href="{{ route('projectKwitansis.edit', $projectKwitansi->id) }}">
+                                                        <i class="fas fa-edit"></i> Edit
+                                                    </a>
+                                                </li>
+                                                <li>
+                                                    <a class="dropdown-item"
+                                                        href="{{ route('catatanKwitansis.create', ['project_kwitansi_id' => $projectKwitansi->id]) }}">
+                                                        <i class="fas fa-sticky-note"></i> Catatan
+                                                    </a>
+                                                </li>
+                                                <li>
+                                                    <a class="dropdown-item"
+                                                        href="{{ route('pekerjaanKwitansis.index', ['project_kwitansi_id' => $projectKwitansi->id]) }}">
+                                                        <i class="fas fa-plus"></i> Tambahkan Pekerjaan
+                                                    </a>
+                                                </li>
+                                                <li>
+                                                    <form
+                                                        action="{{ route('projectKwitansis.destroy', $projectKwitansi->id) }}"
+                                                        method="POST" style="display:inline;">
                                                         @csrf
                                                         @method('DELETE')
-                                                        <button type="submit" class="dropdown-item" onclick="return confirm('Apakah Anda yakin ingin menghapus data ini?')"><i class="fas fa-trash"></i> Hapus</button>
+                                                        <button type="submit" class="dropdown-item"
+                                                            onclick="return confirm('Apakah Anda yakin ingin menghapus data ini?')">
+                                                            <i class="fas fa-trash"></i> Hapus
+                                                        </button>
                                                     </form>
                                                 </li>
+                                                {{-- <li>
+                                                    @php
+                                                        $relationshipExists = \App\Models\BatchKwitansiProjectKwitansi::where(
+                                                            'project_kwitansi_id',
+                                                            $projectKwitansi->id,
+                                                        )->exists();
+                                                        $searchQuery = $projectKwitansi->nomor_surat_jalan; // No encoding
+                                                        $route = route('relationships.index', [
+                                                            'search' => $searchQuery,
+                                                        ]);
+                                                        $label = $relationshipExists
+                                                            ? 'Edit Relationship'
+                                                            : 'Create Relationship';
+                                                    @endphp
+                                                    <a class="dropdown-item" href="{{ $route }}">
+                                                        <i class="fas fa-link"></i> {{ $label }}
+                                                    </a>
+                                                </li> --}}
                                             </ul>
                                         </div>
                                     </td>
