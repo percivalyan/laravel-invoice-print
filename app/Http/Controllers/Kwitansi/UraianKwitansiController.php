a4acc674daa454d038558536d42d3dd6f5f9dd9c
percivalyan <yoryan8888@gmail.com>
2024-11-18 20:07:40 +0700

    new-theme: Updated harga

--- app/Http/Controllers/Kwitansi/UraianKwitansiController.php
+++ app/Http/Controllers/Kwitansi/UraianKwitansiController.php
@@ -71,6 +71,7 @@ class UraianKwitansiController extends Controller
             'jumlah_uraian' => 'nullable|integer',
             'satuan_uraian' => 'nullable|string|max:255',
             'keterangan_uraian' => 'nullable|string',
+            'harga' => 'nullable|numeric',
         ]);
 
         UraianKwitansi::create($request->all());
@@ -108,6 +109,7 @@ class UraianKwitansiController extends Controller
             'jumlah_uraian' => 'nullable|integer',
             'satuan_uraian' => 'nullable|string|max:255',
             'keterangan_uraian' => 'nullable|string',
+            'harga' => 'nullable|numeric',
         ]);
 
         $uraianKwitansi = UraianKwitansi::findOrFail($id);
